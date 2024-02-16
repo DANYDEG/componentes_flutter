288f3e6f686f6cf0004c7eeb0949de1e415c006d
Daniel <danydge5@gmail.com>
2024-02-16 12:07:50 -0600

    Adding Theme

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -10,16 +10,32 @@ class HomeScreen extends StatelessWidget {
         title: const Text('Componentes de flutter'),
         ),
         body: ListView(
-          children: const[
-            Text('Hola mundo'),
-            Text('Hola mundo'),
-            Text('Hola mundo'),
-            Text('Hola mundo'),
-            Text('Hola mundo'),
-            Text('Hola mundo'),
-            Text('Hola mundo'),
-            Text('Hola mundo'),
-        
+          children: [
+            
+            ListTile(
+              leading: const Icon(Icons.accessibility_new_sharp),
+              title: Text(
+                'Hola mundo 1',
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+              subtitle: const Text('Como tan muchacho'),
+              trailing: const Icon(Icons.accessibility_new_rounded),
+            ),  
+
+            const Divider(),      
+            ListTile(
+              leading: const Icon(Icons.accessible_forward_rounded),
+              title: Text('Ta ocupao',style: Theme.of(context).textTheme.headlineLarge,),
+              subtitle: const Text('Yo a ute lo veo muy bien',),
+              trailing: const Icon(Icons.add_reaction_outlined),
+            ),        
+            const Divider(),      
+            ListTile(
+              leading: const Icon(Icons.alternate_email_sharp),
+              title: Text('Hola mundo 3', style: Theme.of(context).textTheme.headlineLarge,),
+              subtitle: const Text('Se merecen una recompensa, velda?'),
+              trailing: const Icon(Icons.assist_walker_sharp),
+            ),        
           ],
         ),
       );
