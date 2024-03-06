5c37b498b3438146bd5e4638f76ff8eb4460ea12
Daniel <danydge5@gmail.com>
2024-03-06 14:00:11 -0600

    Images 2

--- lib/screens/image_screen.dart
+++ lib/screens/image_screen.dart
@@ -11,35 +11,51 @@ class ImagesScreen extends StatefulWidget {
 class _ImagesScreenState extends State<ImagesScreen> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Imagenes', 
-          style: AppTheme.lightTheme.textTheme.bodyMedium,
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Imagenes',
+            style: AppTheme.lightTheme.textTheme.bodyMedium,
+          ),
         ),
-      ),
-      body: ListView(
-        children: [
-        imageCard(),
-
-      ],)
-      
-    );
+        body: ListView(
+          children: [
+            imageCard(),
+            imageWeb(),
+          ],
+        ));
   }
 
-  Card imageCard(){
+  Card imageCard() {
     return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(30)),
-        child: const SizedBox(
-          height: 200,
-          width: 200,
-          child: Image(
-            image:AssetImage('assets/imgs/wallpaperflare.com_wallpaper(1).jpg')
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      margin: const EdgeInsets.all(20),
+      elevation: 10,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30),
+        child: Column(
+          children: [
+            const Image(
+                image: AssetImage('assets/imgs/wallpaperflare.com_wallpaper(1).jpg')
             ),
-          ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                'Porsche Tycan Turbo S',
+                style: AppTheme.lightTheme.textTheme.bodySmall,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+    
 
-      
+  }
+
+  Widget imageWeb(){
+    return Center(
+      child: Image.network('https://i0.wp.com/newspack-yucatan.s3.amazonaws.com/uploads/2020/04/EWk0ww3VAAAjJ6f.jpg?w=600&ssl=1'),
     );
   }
 }
